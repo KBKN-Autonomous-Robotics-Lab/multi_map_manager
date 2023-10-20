0dd04ee238f0227f3479301a6f0bfd79591a0232
koki0624 <koki.kubota.8p@stu.hosei.ac.jp>
2023-10-20 06:49:02 +0000

    Adjust format

--- map_merger/map_merger.py
+++ map_merger/map_merger.py
@@ -4,7 +4,7 @@ from mylib.application import Application
 
 if __name__ == "__main__":
     root = tk.Tk()
-    w, h = root.winfo_screenwidth()-10, root.winfo_screenheight()-100
+    w, h = root.winfo_screenwidth() - 10, root.winfo_screenheight() - 100
     root.geometry("%dx%d+0+0" % (w, h))
     root.title("Multi Map & Waypoints Merger")
     app = Application(root)
